700610111006778d6d96c3d2cb7193aa58d5de4a
Astrophilic <pankajhz99@gmail.com>
2017-03-03 23:24:19 +0530

    Update function.js
    added some extra features!!

--- function.js
+++ function.js
@@ -7,6 +7,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
   console.log('Turning ' + tab.url + ' black!');
   chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="black"'
+    code: 'document.body.style.backgroundColor="red"'
   });
 });
